17f245cdf63a88478836e713ea84b19e2853b0a2
Marcos Nuez Martinez <mnuez@MacBook-Air-de-Marcos.local>
2021-10-06 13:26:40 +0200

    Maquetacion

--- lib/tone_mapping/ToneMapping.cpp
+++ lib/tone_mapping/ToneMapping.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 ToneMapping::ToneMapping(){};
 
-void ToneMapping::clamping(PPMFile &f){
-    for (int i = 0; i < f.height; i++){
-        for (int j = 0;j < f.width; j++){
-            if(f.RGBTuples[i][j].r>1.0){
-                f.RGBTuples[i][j].r=1.0;
+void ToneMapping::clamping(PPMFile &f) {
+    for (int i = 0; i < f.height; i++) {
+        for (int j = 0;j < f.width; j++) {
+            if(f.RGBTuples[i][j].r > 1.0) { 
+                f.RGBTuples[i][j].r = 1.0;
             }
-            if(f.RGBTuples[i][j].g>1.0){
+            if (f.RGBTuples[i][j].g > 1.0) {
                 f.RGBTuples[i][j].g=1.0;
             }
-            if(f.RGBTuples[i][j].b>1.0){
-                f.RGBTuples[i][j].b=1.0;
+            if (f.RGBTuples[i][j].b > 1.0) {
+                f.RGBTuples[i][j].b = 1.0;
             }
         }
     }
