ddd2e33f368cbf65f17a1d16ea176e5422e12b87
Alvaro Echavarri Sola <aechavarris4@gmail.com>
2021-10-06 12:43:37 +0200

    Clamping

--- lib/tone_mapping/ToneMapping.cpp
+++ lib/tone_mapping/ToneMapping.cpp
@@ -7,7 +7,17 @@ ToneMapping::ToneMapping(){};
 
 void ToneMapping::clamping(PPMFile &f){
     for (int i = 0; i < f.height; i++){
-
+        for (int j = 0;j < f.width; j++){
+            if(f.RGBTuples[i][j].r>1.0){
+                f.RGBTuples[i][j].r=1.0;
+            }
+            if(f.RGBTuples[i][j].g>1.0){
+                f.RGBTuples[i][j].g=1.0;
+            }
+            if(f.RGBTuples[i][j].b>1.0){
+                f.RGBTuples[i][j].b=1.0;
+            }
+        }
     }
 };
 
