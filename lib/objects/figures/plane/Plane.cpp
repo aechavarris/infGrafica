39c4893f8747323ab3354eccc76a2954613b58a8
Marcos Nuez Martinez <mnuez@MacBook-Air-de-Marcos.local>
2021-11-04 17:13:23 +0100

    Practica 3 seguimos

--- lib/objects/figures/plane/Plane.cpp
+++ lib/objects/figures/plane/Plane.cpp
@@ -4,20 +4,18 @@
 
 Plane::Plane(){};
 
-Plane::Plane (Point p, Vector nor, RGB rgb) {
+Plane::Plane(Point p, Vector nor, RGB rgb) {
     this->p = p;
     this->normal = nor;
     this->rgb = rgb;
 };
 
 bool Plane::intersect(Ray &ray, float* t1, float *t2) {
-    // assuming vectors are all normalized
-    float denom = this->normal.dot(ray.direction); 
-    if (denom > 1e-6) { 
-        Vec3f p0l0 = p0 - l0; 
-        t = dotProduct(p0l0, n) / denom; 
-        return (t >= 0); 
-    } 
- 
+    float denom = this->normal.dot(ray.direction);
+    if (abs(denom) > 0.0001f) {
+        Point p = Point(this->p.x - ray.origin.x, this->p.y - ray.origin.y, this->p.z - ray.origin.z);
+        float t = p.dot(this->normal) / denom;
+        if (t >= 0) return true;
+    }
     return false;
 }
